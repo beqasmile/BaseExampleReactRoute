0b382af4bd4e048eca542a5bbd59adad012854df
ilya <beqasmile@gmail.com>
2020-09-06 19:33:16 +0300

    add match

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 
 
+const Courses = ({ match }) => (
+  <div>
+     <ul>
+        <li><Link to={`${match.url}/technology`}>Technology</Link></li>
+        <li><Link to={`${match.url}/business`}>Business</Link></li>
+        <li><Link to={`${match.url}/economics`}>Economics</Link></li>
+    </ul>
+
+
+    <Route exact path={`${match.path}/technology`} render={() => (<div> This is technology </div>)}/>
+    <Route path={`${match.path}/business`} component={() => (<div> This is business </div>)}/>
+    <Route path={`${match.path}/economics`} component={() => (<div> This is economics </div>)}/>
+  </div>
+);
+
+
+
 const Home = () => (
   <div>
     <h2> Home </h2>
@@ -29,7 +46,7 @@ const City = () => (
   </div>
 );
 
-class App extends Component {
+class Cities extends Component {
   render() {
     return (
       <div>
@@ -41,6 +58,25 @@ class App extends Component {
 
         <Route path="/" component={Home}/>
         <Route path="/airports" component={Airport}/>
+        <Route path="/cities" component={City}/>
+      </div>
+    );
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/courses">Courses</Link></li>
+          <li><Link to="/cities">Cities</Link></li>
+        </ul>
+
+        <Route path="/" exact component={Home}/>
+        <Route path="/courses" component={Courses}/>
         <Route path="/cities" component={City}/>
       </div>
     );
